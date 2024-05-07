ca2db3e99ca0efb6bbc8ddd74cc0f328dfd867e8
yeslee <yeseull726@gmail.com>
2024-05-07 22:56:37 +0900

    Start Project "GOTEAT"

---
+++ goteat/src/main/java/javaeatsong/goteat/GoteatApplication.java
@@ -0,0 +1,13 @@
+package javaeatsong.goteat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoteatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoteatApplication.class, args);
+	}
+
+}
---
+++ goteat/src/test/java/javaeatsong/goteat/GoteatApplicationTests.java
@@ -0,0 +1,13 @@
+package javaeatsong.goteat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoteatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
