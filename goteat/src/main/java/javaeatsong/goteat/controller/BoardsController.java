569bc529012cb246f2bd187b209c95ef69e7dd6f
yeslee <yeseull726@gmail.com>
2024-05-18 22:43:31 +0900

    FEAT: 전체 소분 내역 조회 및 검색 기능 API 구현 #1

--- goteat/src/main/java/javaeatsong/goteat/controller/BoardsController.java
+++ goteat/src/main/java/javaeatsong/goteat/controller/BoardsController.java
@@ -1,13 +1,15 @@
 package javaeatsong.goteat.controller;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javaeatsong.goteat.model.Boards;
 import javaeatsong.goteat.service.BoardsService;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -20,22 +22,14 @@ public class BoardsController {
 		this.boardsService = boardsService;
 	}
 
-	// 참고용 테스트 코드. 삭제 가능
 	@GetMapping("/board")
-	public List<Boards> getBoardsAll() throws Exception {
-		return boardsService.getBoardsAll();
+	public List<HashMap<String, Object>> getBoard(@RequestHeader HttpHeaders header) throws Exception {
+		return boardsService.getBoard(header.getFirst("uid"), "", null);
 	}
 
-	// 참고용 테스트 코드. 삭제 가능
-	@GetMapping("/board/insert-test")
-	public void insert(Boards board) throws Exception {
-		boardsService.insert(board);
+	@GetMapping("/board/search")
+	public List<HashMap<String, Object>> getBoardByKeywordCategories(@RequestParam("keyword") String keyword,
+			@RequestParam("categories") List<String> categories, @RequestHeader HttpHeaders header) throws Exception {
+		return boardsService.getBoard(header.getFirst("uid"), keyword, categories);
 	}
-
-	// 참고용 테스트 코드. 삭제 가능
-	@GetMapping("/board/{id}")
-	public Boards getUserProfile(@PathVariable("id") String id) throws Exception {
-		return boardsService.getBoard(Integer.valueOf(id));
-	}
-
 }
--- goteat/src/main/java/javaeatsong/goteat/repository/BoardsMapper.java
+++ goteat/src/main/java/javaeatsong/goteat/repository/BoardsMapper.java
@@ -1,14 +1,17 @@
 package javaeatsong.goteat.repository;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import javaeatsong.goteat.model.Boards;
 
 @Mapper
 public interface BoardsMapper {
-	List<Boards> selectList() throws Exception;
+	List<HashMap<String, Object>> selectList(@Param("uid") String uid, @Param("keyword") String keyword,
+			@Param("categories") List<String> categories) throws Exception;
 
 	Boards select(int id) throws Exception;
 
