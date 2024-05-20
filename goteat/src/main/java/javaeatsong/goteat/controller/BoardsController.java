6095659afbcc0b800abe198af28e983f413516a9
solux <leehn45@naver.com>
2024-05-21 02:32:05 +0900

    FEAT: 소분 등록 기능 구현 #6
    - 기존에 작성했던 API 명세서의 request body를 스네이크 케이스로 썼었는데, 매핑 오류로 카멜 케이스로 바꿔 해결. 해당 부분 괜찮을지 검토 필요

--- goteat/src/main/java/javaeatsong/goteat/controller/BoardsController.java
+++ goteat/src/main/java/javaeatsong/goteat/controller/BoardsController.java
@@ -2,16 +2,25 @@ package javaeatsong.goteat.controller;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import javaeatsong.goteat.service.BoardsService;
+import javaeatsong.goteat.model.Boards;
 
 import java.util.HashMap;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @RestController
 @MapperScan(basePackages = "javaeatsong.goteat.repository") // 탐색할 패키지 설정
@@ -39,5 +48,18 @@ public class BoardsController {
 			@RequestHeader("uid") String uid) throws Exception {
 		return boardsService.getBoardDetail(uid, bid);
 	}
+	
+	@PostMapping("/board")
+	   public ResponseEntity<String> postBoard(@RequestBody Boards board, @RequestHeader("uid") String uid) throws Exception {
+			board.setUserId(Integer.valueOf(uid));
+			
 
+		    // 로깅
+		    Logger logger = LoggerFactory.getLogger(getClass());
+		    logger.info("Received board data: {}", board);
+		    
+		    
+			boardsService.postBoard(board);
+	       return ResponseEntity.status(HttpStatus.CREATED).body("Board created successfully");
+    }
 }
