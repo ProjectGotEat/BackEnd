c2ea2da14060c59fb1111c369ce327dcae3826cb
solux <leehn45@naver.com>
2024-05-27 00:49:28 +0900

    FEAT: 나의 리뷰 불러오기 API 구현 #15

---
+++ goteat/src/main/java/javaeatsong/goteat/controller/ReviewsController.java
@@ -0,0 +1,36 @@
+package javaeatsong.goteat.controller;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javaeatsong.goteat.service.ReviewsService;
+import javaeatsong.goteat.model.Reviews;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@MapperScan(basePackages = "javaeatsong.goteat.repository") // 탐색할 패키지 설정
+public class ReviewsController {
+
+	private final ReviewsService reviewsService;
+
+	public ReviewsController(ReviewsService reviewsService) {
+		this.reviewsService = reviewsService;
+	}
+
+	@GetMapping("/user/review")
+	public List<HashMap<String, Object>> getReview(@RequestHeader HttpHeaders header) throws Exception {
+		return reviewsService.getReview(header.getFirst("uid"));
+	}
+}
