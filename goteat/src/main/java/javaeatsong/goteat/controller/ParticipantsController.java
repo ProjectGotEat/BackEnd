256d4c6c325d49ad88a0ba1769acc5b3cb5b9992
hujumee <hujumee27@gmail.com>
2024-05-28 22:14:44 +0900

    REFACTOR: 리턴 값이 없는 경우 204 리턴하도록 수정

--- goteat/src/main/java/javaeatsong/goteat/controller/ParticipantsController.java
+++ goteat/src/main/java/javaeatsong/goteat/controller/ParticipantsController.java
@@ -43,20 +43,35 @@ public class ParticipantsController {
 
 	// 내가 주최한 소분 전체 조회
 	@GetMapping("/participant/organize")
-	public List<HashMap<String, Object>> getPariticpantsOrganized(@RequestHeader("uid") int uid) throws Exception {
-		return participantsService.getParticipantsOrganized(uid);
+	public ResponseEntity<List<HashMap<String, Object>>> getPariticpantsOrganized(@RequestHeader("uid") int uid) throws Exception {
+		List<HashMap<String, Object>> response_data = participantsService.getParticipantsOrganized(uid);
+		if (response_data.size() > 0) {
+			return ResponseEntity.status(HttpStatus.OK).body(response_data);
+		} else {
+			return ResponseEntity.noContent().build();
+		}
 	}
 
 	// 내가 참여한 소분 전체 조회
 	@GetMapping("/participant/participate")
-	public List<HashMap<String, Object>> getParticipantsParticipating(@RequestHeader("uid") int uid) throws Exception {
-		return participantsService.getParticipantsParticipating(uid);
+	public ResponseEntity<List<HashMap<String, Object>>> getParticipantsParticipating(@RequestHeader("uid") int uid) throws Exception {
+		List<HashMap<String, Object>> response_data = participantsService.getParticipantsParticipating(uid);
+		if (response_data.size() > 0) {
+			return ResponseEntity.status(HttpStatus.OK).body(response_data);
+		} else {
+			return ResponseEntity.noContent().build();
+		}
 	}
 
 	// 종료된 소분 전체 조회
 	@GetMapping("/participant/end")
-	public List<HashMap<String, Object>> getParticipantsEnded(@RequestHeader("uid") int uid) throws Exception {
-		return participantsService.getParticipantsEnded(uid);
+	public ResponseEntity<List<HashMap<String, Object>>> getParticipantsEnded(@RequestHeader("uid") int uid) throws Exception {
+		List<HashMap<String, Object>> response_data = participantsService.getParticipantsEnded(uid);
+		if (response_data.size() > 0) {
+			return ResponseEntity.status(HttpStatus.OK).body(response_data);
+		} else {
+			return ResponseEntity.noContent().build();
+		}
 	}
 
 	// 1:1 쪽지 상세 조회 구현
@@ -66,4 +81,5 @@ public class ParticipantsController {
 			@RequestHeader("uid") int uid) throws Exception {
 		return participantsService.getParticipantMessages(id, uid);
 	}
+
 }
