d86b059ac8fcf3444a1293ff5177443357b070e1
hujumee <hujumee27@gmail.com>
2024-05-28 12:36:30 +0900

    FIX: merge conflict 해결

--- goteat/src/main/java/javaeatsong/goteat/controller/ParticipantsController.java
+++ goteat/src/main/java/javaeatsong/goteat/controller/ParticipantsController.java
@@ -1,10 +1,17 @@
 package javaeatsong.goteat.controller;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 import javaeatsong.goteat.model.Participants;
 import javaeatsong.goteat.service.ParticipantsService;
@@ -20,6 +27,18 @@ public class ParticipantsController {
 
 	public ParticipantsController(ParticipantsService participantsService) {
 		this.participantsService = participantsService;
+	}
+
+	@PostMapping("/board/{id}/request")
+	public ResponseEntity<String> postParticipant(@RequestBody Participants participant, @RequestHeader("uid") String uid, @PathVariable("id") int bid) throws Exception {
+		participant.setUserId(Integer.valueOf(uid));
+		participant.setBoardId(Integer.valueOf(bid));
+		int result = participantsService.postParticipant(participant);
+		if (result > 0) {
+			return ResponseEntity.status(HttpStatus.CREATED).body("requested successfully");
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("no remain_headcnt. remain_headcnt is 0");
+		}
 	}
 
 	// 내가 주최한 소분 전체 조회
