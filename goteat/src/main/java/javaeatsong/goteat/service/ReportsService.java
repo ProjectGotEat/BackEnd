47a303853db7133bf56b1d863bbd02ea9af8d8bc
solux <leehn45@naver.com>
2024-05-31 23:25:10 +0900

    FEAT: 신고하기 기능 구현 #31

---
+++ goteat/src/main/java/javaeatsong/goteat/service/ReportsService.java
@@ -0,0 +1,18 @@
+package javaeatsong.goteat.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javaeatsong.goteat.repository.ReportsMapper;
+import javaeatsong.goteat.model.Reports;
+
+@Service
+public class ReportsService {
+
+	@Autowired
+	private ReportsMapper reportsMapper;
+    
+	public void postReport(Reports report) throws Exception {
+        reportsMapper.insert(report);
+    }
+}
--- goteat/src/main/java/javaeatsong/goteat/service/ReviewsService.java
+++ goteat/src/main/java/javaeatsong/goteat/service/ReviewsService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javaeatsong.goteat.repository.ReviewsMapper;
-import javaeatsong.goteat.model.Boards;
 import javaeatsong.goteat.model.Reviews;
 
 @Service
