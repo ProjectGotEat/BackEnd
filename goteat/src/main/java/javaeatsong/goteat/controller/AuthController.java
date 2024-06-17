0b8807418e8e19c15b1fcf9c6d89179803c3d663
yeslee <yeseull726@gmail.com>
2024-06-18 01:00:19 +0900

    FEAT: 프로필 사진 업로드 기능 추가 #50

--- goteat/src/main/java/javaeatsong/goteat/controller/AuthController.java
+++ goteat/src/main/java/javaeatsong/goteat/controller/AuthController.java
@@ -1,44 +1,61 @@
 package javaeatsong.goteat.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javaeatsong.goteat.model.Users;
 import javaeatsong.goteat.service.AuthService;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class AuthController {
 
+	@Value("${file.upload-dir}")
+	private String uploadDir;
+	
 	private final AuthService authService;
 
 	public AuthController(AuthService authService) {
 		this.authService = authService;
 	}
 
-	@PostMapping("/auth/join")
-	public ResponseEntity<String> postAuthJoin(@RequestBody Map<String, ?> requestBody) throws Exception {
-		if (requestBody.get("name") == null || requestBody.get("profile_name") == null
-				|| requestBody.get("email") == null || requestBody.get("password") == null
-				|| requestBody.get("noti_allow") == null) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("request faild");
-		}
-
-		Users user = new Users();
-		user.setName(requestBody.get("name").toString());
-		user.setProfileName(requestBody.get("profile_name").toString());
-		user.setEmail(requestBody.get("email").toString());
-		user.setPassword(requestBody.get("password").toString());
+	@PostMapping(value = "/auth/join", consumes = "multipart/form-data")
+	public ResponseEntity<String> postAuthJoin(@RequestPart("profile_image") MultipartFile profileImage, @RequestPart("user") String userJson) throws Exception {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Users user = objectMapper.readValue(userJson, Users.class);
 		user.setRank("씨앗");
 		user.setPoint(0);
-		user.setNotiAllow((Integer) requestBody.get("noti_allow"));
+		
+		// 파일 처리
+		if (!profileImage.isEmpty()) {
+			try {
+				Path path = Paths.get(uploadDir + File.separator + profileImage.getOriginalFilename());
+				Files.write(path, profileImage.getBytes());
+				String fileUrl = "http://goteat-project-goteat-fbd23032.koyeb.app/uploads/"
+						+ profileImage.getOriginalFilename();
+				user.setImage(fileUrl);
+			} catch (IOException e) {
+				e.printStackTrace();
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload item_image1");
+			}
+		}
 
 		authService.postAuthJoin(user);
 
